10ca002af9ae32b0b68fa2e8541fd3b0da12ed7e
Casey Harford <casey@caseyharford.com>
2012-02-07 23:16:56 -0800

    removed json scaffold from hiring managers controller

--- app/controllers/evaluators_controller.rb
+++ app/controllers/evaluators_controller.rb
@@ -7,7 +7,6 @@ class EvaluatorsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @evaluators }
     end
   end
 
@@ -18,7 +17,6 @@ class EvaluatorsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @evaluator }
     end
   end
 
@@ -29,7 +27,6 @@ class EvaluatorsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @evaluator }
     end
   end
 
@@ -46,10 +43,8 @@ class EvaluatorsController < ApplicationController
     respond_to do |format|
       if @evaluator.save
         format.html { redirect_to @evaluator, notice: 'Evaluator was successfully created.' }
-        format.json { render json: @evaluator, status: :created, location: @evaluator }
       else
         format.html { render action: "new" }
-        format.json { render json: @evaluator.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -62,10 +57,8 @@ class EvaluatorsController < ApplicationController
     respond_to do |format|
       if @evaluator.update_attributes(params[:evaluator])
         format.html { redirect_to @evaluator, notice: 'Evaluator was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @evaluator.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -78,7 +71,6 @@ class EvaluatorsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to evaluators_url }
-      format.json { head :no_content }
     end
   end
   private
--- app/controllers/hiring_managers_controller.rb
+++ app/controllers/hiring_managers_controller.rb
@@ -6,7 +6,6 @@ class HiringManagersController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @hiring_managers }
     end
   end
 
@@ -17,7 +16,6 @@ class HiringManagersController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @hiring_manager }
     end
   end
 
@@ -28,7 +26,6 @@ class HiringManagersController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @hiring_manager }
     end
   end
 
@@ -45,10 +42,8 @@ class HiringManagersController < ApplicationController
     respond_to do |format|
       if @hiring_manager.save
         format.html { redirect_to @hiring_manager, notice: 'Hiring manager was successfully created.' }
-        format.json { render json: @hiring_manager, status: :created, location: @hiring_manager }
       else
         format.html { render action: "new" }
-        format.json { render json: @hiring_manager.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -61,10 +56,8 @@ class HiringManagersController < ApplicationController
     respond_to do |format|
       if @hiring_manager.update_attributes(params[:hiring_manager])
         format.html { redirect_to @hiring_manager, notice: 'Hiring manager was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @hiring_manager.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -77,7 +70,6 @@ class HiringManagersController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to hiring_managers_url }
-      format.json { head :no_content }
     end
   end
 end

81d052046b3e2c7fff191dba4150dc2c4d5c6eec
Casey Harford <casey@caseyharford.com>
2012-03-31 21:30:53 -0700

    added destroy dependent for answers to questions, this way we dont corrupt data when deleting a question on an application which already has answers. This should never happen, but if it does, no damage to db will be done

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
 	belongs_to :jobapp
-	# has_many   :answers
+	has_many   :answers, :dependent => :destroy
 	# validates :job_id, :presence => true
 	# validates :body,   :presence => true
 	attr_accessible :job_id, :body, :qtype, :jobapp_id, :choice_a, :choice_b, :choice_c, :choice_d

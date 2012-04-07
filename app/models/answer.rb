4bdd52da86f074da7cb37d7502aa6174605dd04a
Casey Harford <casey@caseyharford.com>
2012-04-07 14:35:21 -0700

    added photo attachment to references, also removed unneeded attributes fron attr_accessible for answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,5 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :applicant
   has_attached_file :photo, :path => "resumes/:id/:basename.:extension"
-  attr_accessible :question_id, :applicant_id, :body, :photo_file_name, :photo_content_type, :photo_file_size, :photo
+  attr_accessible :question_id, :applicant_id, :body, :photo
 end
--- app/models/reference.rb
+++ app/models/reference.rb
@@ -2,12 +2,12 @@ class Reference < ActiveRecord::Base
   belongs_to :user
   validates  :user_id,        :presence => true
   validates	 :firstname,      :presence => true
-  validates  :lastname,       :presence => true
-  validates	 :letter,         :presence => true
+  #validates  :lastname,       :presence => true
+  #validates	 :letter,         :presence => true
   validates  :reference_hash, :presence => true
   has_attached_file :photo, :path => "references/:id/:basename.:extension"
-  attr_accessible :user_id, :firstname, :lastname, :email, :phone, :letter, :reference_hash
-	#email and phone not required
+  attr_accessible :user_id, :firstname, :lastname, :email, :phone, :letter, :reference_hash, :photo
+	#lastname, letter, email, attachment, and phone not required
 
   def request_for_reference
     @subject = "Email sent via your website"

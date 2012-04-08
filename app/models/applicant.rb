74e5985925d8a32e2e6272b9f21eb42c4ea549dd
Casey Harford <casey@caseyharford.com>
2012-04-07 19:42:01 -0700

    changed header background on tables

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -3,11 +3,12 @@ class Applicant < ActiveRecord::Base
   belongs_to :job
   has_many :ratings, :dependent => :destroy
   has_many :answers, :dependent => :destroy
+
   accepts_nested_attributes_for :answers
   validates :user_id, :uniqueness => { :scope => :job_id},
                       :presence   => true
   validates :job_id,  :presence => true
-  has_many :additional_facts
+  has_many :additional_facts, :dependent => :destroy
   attr_accessible :user_id, :job_id, :published, :answers_attributes
    def to_param
     @user = User.find(user_id)
